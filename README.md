8fc76f16c96056a23dd1e3432c96ae56defb38f4
thom-s <45467550+thom-s@users.noreply.github.com>
2024-02-27 15:19:11 -0500

    New version

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # ff-image-options
-Firefox extension that adds options to the context menu to open an image in a new tab or to search it on Google.
+Firefox extension that adds options to the context menu to open an image in a new tab or to search it on various search engines.
+
+Currently supported engines:
+- Google Lens
+- Bing
+- TinEye
+- Yandex
+- ImgOps
 
 Get the addon : https://addons.mozilla.org/en-US/firefox/addon/image-options/
--- background.js
+++ background.js
@@ -1,15 +1,44 @@
-var search_domain = "http://images.google.com/searchbyimage?site=search&image_url="
+var search_domain_google = "https://lens.google.com/uploadbyurl?url="
+var search_domain_bing   = "https://www.bing.com/images/searchbyimage?cbir=sbi&imgurl="
+var search_domain_tineye = "https://www.tineye.com/search?url="
+var search_domain_yandex = "https://yandex.com/images/search?rpt=imageview&url="
+var search_domain_imgops = "https://imgops.com/"
+
 
 browser.menus.create({
-  id: "google_search",
-  title: "Search Image on Google",
-  contexts: ["image"]
+    id: "open_new_tab",
+    title: "Open Image in New Tab",
+    contexts: ["image"]
 });
 
 browser.menus.create({
-  id: "open_new_tab",
-  title: "Open Image in New Tab",
-  contexts: ["image"]
+    id: "google_search",
+    title: "Search Image on Google",
+    contexts: ["image"]
+});
+
+browser.menus.create({
+    id: "bing_search",
+    title: "Search Image on Bing",
+    contexts: ["image"]
+});
+
+browser.menus.create({
+    id: "tineye_search",
+    title: "Search Image on TinEye",
+    contexts: ["image"]
+});
+
+browser.menus.create({
+    id: "yandex_search",
+    title: "Search Image on Yandex",
+    contexts: ["image"]
+});
+
+browser.menus.create({
+    id: "imgops_search",
+    title: "Search Image on ImgOps",
+    contexts: ["image"]
 });
 
 browser.menus.onClicked.addListener((info, tab) => {
@@ -20,7 +49,27 @@ browser.menus.onClicked.addListener((info, tab) => {
         break;
 
     case "google_search":
-        search_url = search_domain + target;
+        search_url = search_domain_google + target;
+        browser.tabs.create({url:search_url});
+        break;
+
+    case "bing_search":
+        search_url = search_domain_bing + target;
+        browser.tabs.create({url:search_url});
+        break;
+
+    case "tineye_search":
+        search_url = search_domain_tineye + target;
+        browser.tabs.create({url:search_url});
+        break;
+
+    case "yandex_search":
+        search_url = search_domain_yandex + target;
+        browser.tabs.create({url:search_url});
+        break;
+
+    case "imgops_search":
+        search_url = search_domain_imgops + target;
         browser.tabs.create({url:search_url});
         break;
 
